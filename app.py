3888d1c0c2052b12c0120ca087def73b27598436
Ezra <ezdeutsch@gmail.com>
2021-10-07 15:13:03 -0400

    updated app.py

--- app.py
+++ app.py
@@ -72,11 +72,24 @@ def stats(start=None, end=None):
         results = session.query(*sel).\
             filter(Measurement.date >= start).all()
         temps = list(np.ravel(results))
-        return jsonify(temps=temps)
-    
+        return jsonify(temps)
+
     results = session.query(*sel).\
         filter(Measurement.date >= start).\
         filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
-    return jsonify(temps)
+    return jsonify(temps=temps)
+
+
+    # if not end:
+    #     results = session.query(*sel).\
+    #         filter(Measurement.date >= start).all()
+    #     temps = list(np.ravel(results))
+    #     return jsonify(temps=temps)
+    
+    # results = session.query(*sel).\
+    #     filter(Measurement.date >= start).\
+    #     filter(Measurement.date <= end).all()
+    # temps = list(np.ravel(results))
+    # return jsonify(temps)
 
